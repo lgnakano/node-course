2404821bc421da6b75f300321f3059a595a56e34
Luis Nakano <lgnakano@gmail.com>
2019-03-09 12:46:22 -0500

    Use appendFileSync

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,5 @@
 var fs=require('fs')
-fs.writeFileSync('notes.txt','This file was created by node.js!')
+
+fs.writeFileSync('notes.txt','My name is bob.')
+
+fs.appendFileSync('notes.txt',' This is an appended line.')
