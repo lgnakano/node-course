f348c9d7de08c4493bd57623efceb9e45205be10
Luis Nakano <lgnakano@gmail.com>
2019-03-17 22:23:38 -0400

    JSON  parsing, modify, then stringfy, saving to the filej.

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,11 +1,23 @@
 const fs = require('fs')
-const book = {
+/* const book = {
     title: 'Ego is the Enemy',
     author: 'Ryan Holiday'
 }
 
 const bookJSON = JSON.stringify(book)
 fs.writeFileSync('1-json.json', bookJSON)
+*/
 
-const parseData = JSON.parse(bookJSON)
-console.log(parseData.author)
+const dataBuffer = fs.readFileSync('1-json.json')
+console.log(dataBuffer)
+const dataJSON = dataBuffer.toString()
+console.log(dataJSON)
+
+const parseData = JSON.parse(dataJSON)
+parseData.name='Luis'
+parseData.age='49'
+console.log(parseData)
+
+const newDataJSON = JSON.stringify(parseData)
+fs.writeFileSync('1-json.json', newDataJSON)
+
