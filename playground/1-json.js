7909319b6c173d8f2f578c697b649862086c84a7
Luis Nakano <lgnakano@gmail.com>
2019-03-17 21:51:33 -0400

    Parsing/stringfying json objects.

---
+++ playground/1-json.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const book = {
+    title: 'Ego is the Enemy',
+    author: 'Ryan Holiday'
+}
+
+const bookJSON = JSON.stringify(book)
+console.log(bookJSON)
+
+const parseData = JSON.parse(bookJSON)
+console.log(parseData.author)
