2a532fe72252df456be4bdd310cead57d8e93caf
Luis Nakano <lgnakano@gmail.com>
2019-03-17 21:11:53 -0400

    Use yargs to parse command line commands.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,46 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
+// customize yargs version
+yargs.version('1.1.0')
 
-console.log(msg)
+// create add command
+yargs.command({
+    command:'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note!')
+    }
+})
 
-console.log(chalk.green.inverse.bold('Success'))
+// create remove command
+yargs.command({
+    command:'remove',
+    describe: 'Remove a new note',
+    handler: function(){
+        console.log('Removing the note!')
+    }
+})
 
+// create list command
+yargs.command({
+    command:'list',
+    describe: 'List all note',
+    handler: function(){
+        console.log('Listing all notes!')
+    }
+})
+
+// create read command
+yargs.command({
+    command:'read',
+    describe: 'Reading the note',
+    handler: function(){
+        console.log('Reading the note!')
+    }
+})
+
+
+// add, remove, read, list
+console.log(yargs.argv)
