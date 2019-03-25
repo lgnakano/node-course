4b6749595f4d8675898fd943eb23a02e8494f89e
Luis Nakano <lgnakano@gmail.com>
2019-03-25 13:57:26 -0400

    Use find function for optimization and use white for Your Note(s)

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -18,25 +18,25 @@ const saveNotes = (notes) => {
 }
 
 const addNote = (title, body) => {
-    var notes = loadNotes()
+    const notes = loadNotes()
     console.log(notes)
-    var note = {
+    const note = {
         title,
         body
     }
-    var duplicateNotes = notes.filter((note) => note.title === title)
-    if (duplicateNotes.length === 0) {
+    
+    const duplicateNote = notes.find((note) => note.title === title)
+    if (!duplicateNote) {
         notes.push(note)
         saveNotes(notes)
         console.log(chalk.green.inverse('New note added!'))
-        return note
     } else {
         console.log(chalk.red.inverse('Note title taken!'))
     }
 }
 const listNotes = () => {
 
-    console.log(chalk.green.inverse('Your notes'))
+    console.log(chalk.white.inverse('Your notes'))
     const allNotes = loadNotes() 
     allNotes.forEach((element) => {
         console.log(chalk.green.inverse('Title'),element.title)
@@ -47,14 +47,14 @@ const listNotes = () => {
 var getNote = (title) => {
     const allNotes = loadNotes()
     console.log(allNotes)
-    var myNote = allNotes.filter((note) => note.title===title)
+    var myNote = allNotes.find((note) => note.title===title)
     console.log(myNote)
     if(myNote.length === 0) {
         console.log(chalk.red.inverse('Note not found!'))
     } else {
-        console.log(chalk.green.inverse('Your note'))
-        console.log(chalk.green.inverse('Title'),myNote[0].title)
-        console.log(chalk.green('Body'), myNote[0].body)
+        console.log(chalk.white.inverse('Your note'))
+        console.log(chalk.green.inverse('Title'),myNote.title)
+        console.log(chalk.green('Body'), myNote.body)
     }
 }
 
