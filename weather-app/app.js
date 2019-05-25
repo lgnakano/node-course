b8e45ad2033cee38da2f7f9358b4f2eedab54592
Luis Nakano <lgnakano@gmail.com>
2019-05-25 19:53:28 -0400

    Obtain weather in JSON format.

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/5f6372bd5774f2dfce220a88a3c70e4f/37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
 })
