d95f3b7b77df4c92e8745bc163043ae46ad94183
Luis Nakano <lgnakano@gmail.com>
2019-05-26 10:26:08 -0400

    Geocode as a callback.

--- weather-app/app.js
+++ weather-app/app.js
@@ -12,17 +12,43 @@ const request = require('request')
 //     }
 // })
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGduYWthbm8iLCJhIjoiY2p1NGZkbjF5MHhnODQ0b2JreGE1b3d0YSJ9.w7JC7gDqQcVNQ0zo-E8AyA&limit=1'
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGduYWthbm8iLCJhIjoiY2p1NGZkbjF5MHhnODQ0b2JreGE1b3d0YSJ9.w7JC7gDqQcVNQ0zo-E8AyA&limit=1'
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to location services!')
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location. Try another search.')
-    } else {
-        const longitude = response.body.features[0].center[0] 
-        const latitude = response.body.features[0].center[1]
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to location services!')
+//     } else if (response.body.features.length === 0) {
+//         console.log('Unable to find location. Try another search.')
+//     } else {
+//         const longitude = response.body.features[0].center[0] 
+//         const latitude = response.body.features[0].center[1]
 
-        console.log(latitude, longitude)
-    }
+//         console.log(latitude, longitude)
+//     }
+// })
+
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
+    + encodeURIComponent( address )
+    +'.json?access_token=pk.eyJ1IjoibGduYWthbm8iLCJhIjoiY2p1NGZkbjF5MHhnODQ0b2JreGE1b3d0YSJ9.w7JC7gDqQcVNQ0zo-E8AyA&limit=1'
+ 
+    request({url: url, json: true}, (error, response) => {
+        if(error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (response.body.features.length === 0) {
+            callback('Unable to lind location. Try another search', undefined)
+        } else {
+            callback(undefined, {
+                longitude: response.body.features[0].center[0],
+                latitude: response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+geocode("Tampa",(error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
