5033644028cd92c227e5ed6aed8234c171705b6f
Luis Nakano <lgnakano@gmail.com>
2019-05-26 10:33:45 -0400

    Move geocode in its own file.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,4 @@
-const request = require('request')
+const geocode = require('./utils/geocode')
 
 // const url = 'https://api.darksky.net/forecast/5f6372bd5774f2dfce220a88a3c70e4f/37.8267,-122.4233'
 
@@ -28,25 +28,6 @@ const request = require('request')
 // })
 
 
-const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
-    + encodeURIComponent( address )
-    +'.json?access_token=pk.eyJ1IjoibGduYWthbm8iLCJhIjoiY2p1NGZkbjF5MHhnODQ0b2JreGE1b3d0YSJ9.w7JC7gDqQcVNQ0zo-E8AyA&limit=1'
- 
-    request({url: url, json: true}, (error, response) => {
-        if(error) {
-            callback('Unable to connect to location services!', undefined)
-        } else if (response.body.features.length === 0) {
-            callback('Unable to lind location. Try another search', undefined)
-        } else {
-            callback(undefined, {
-                longitude: response.body.features[0].center[0],
-                latitude: response.body.features[0].center[1],
-                location: response.body.features[0].place_name
-            })
-        }
-    })
-}
 
 geocode("Tampa",(error, data) => {
     console.log('Error', error)
