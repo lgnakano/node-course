fe196192fedfdd764562c326f2df721274457df0
Luis Nakano <lgnakano@gmail.com>
2019-05-25 20:06:27 -0400

    Add request to mapbox.

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,3 +5,14 @@ const url = 'https://api.darksky.net/forecast/5f6372bd5774f2dfce220a88a3c70e4f/3
 request({ url: url, json: true }, (error, response) => {
     console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
 })
+
+// Geocoding
+// Address -> Lat/Long -> Weather
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGduYWthbm8iLCJhIjoiY2p1NGZkbjF5MHhnODQ0b2JreGE1b3d0YSJ9.w7JC7gDqQcVNQ0zo-E8AyA&limit=1'
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    const latitude = response.body.features[0].center[0]
+    const longitude = response.body.features[0].center[1]
+    console.log(latitude, longitude)
+})
