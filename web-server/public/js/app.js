ebf0e3fe633c541cd8b1261dd524eeba18d383e3
Luis Nakano <lgnakano@gmail.com>
2019-05-27 19:52:28 -0400

    Add rendering of forecast, location, and error messages.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -7,6 +7,9 @@ const weatherForm = document.querySelector('form')
 
 const search = document.querySelector('input')
 
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
@@ -18,8 +21,12 @@ weatherForm.addEventListener('submit', (e) => {
             response.json()
                 .then((data) => {
                     if (data.error) {
+                        messageOne.textContent = data.error
+                        messageTwo.textContent =''
                         console.log(data.error)
                     } else {
+                        messageOne.textContent = ''
+                        messageTwo.textContent = data.location+': '+data.forecast
                         console.log(data.location)
                         console.log(data.forecast)
                     }
