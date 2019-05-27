1cb261378f24e4ab9152b0b996218e0c6e0e2bbd
Luis Nakano <lgnakano@gmail.com>
2019-05-27 17:51:03 -0400

    Search for weather by location only when form is submitted.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,22 +1,30 @@
 console.log('Client side javascript file is loaded!')
 
-// fetch('http://puzzle.mead.io/puzzle')
-//     .then((response) => (
-//         response.json()
-//             .then((data)=>{
-//                 console.log(data)
-//             })
-//     ))
 
-fetch('http://localhost:3000/weather?address=Tampa')
-    .then((response) => {
-        response.json()
-            .then((data) => {
-                if (data.error) {
-                    console.log(data.error)
-                } else {
-                    console.log(data.location)
-                }
 
-            })
-    })
+
+const weatherForm = document.querySelector('form')
+
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+
+    const url = 'http://localhost:3000/weather?address=' + location;
+
+    fetch(url)
+        .then((response) => {
+            response.json()
+                .then((data) => {
+                    if (data.error) {
+                        console.log(data.error)
+                    } else {
+                        console.log(data.location)
+                        console.log(data.forecast)
+                    }
+
+                })
+        })
+
+})
